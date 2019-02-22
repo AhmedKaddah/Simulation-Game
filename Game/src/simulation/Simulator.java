d3c8ea65aacc1c47fcf16155bcb9d175dfd9ac1b
camirost <47898898+camirost@users.noreply.github.com>
2019-02-22 23:13:51 +0200

    t

--- Game/src/simulation/Simulator.java
+++ Game/src/simulation/Simulator.java
@@ -12,11 +12,16 @@ private int currentCycle;
 private ArrayList<ResidentialBuilding> buildings;
 private ArrayList<Citizen> citizens;
 private ArrayList<Unit> emergencyUnits;
-//private ArrayList<Disaster> plannedDisasters;
+private ArrayList<Disaster> plannedDisasters;
 private ArrayList<Disaster> executedDisasters;
 private Address[][]world;
 
 	public Simulator() {
-		
+		world= new Address[10][10];
+		for(int i=1; i<=10;i++) {
+			for(int j=1; j<=10;j++)
+				world[i][j]= new Address(i,j);			
+		}
+	//  The ArrayLists of ResidentialBuildings, Citizens, emergencyUnits and plannedDisasters are initially loaded from csv files.
 	}
 }
