4179163ad2d827cca9b175d259ab1ca37138ecf0
TOSHIBA <TOSHIBA@Mazen-Shehab>
2019-02-22 22:51:41 +0200

    klji

---
+++ Game/src/model/units/FireTruck.java
@@ -0,0 +1,8 @@
+package model.units;
+import simulation.Address;
+
+public class FireTruck extends FireUnit {
+	public FireTruck(String id, Address location, int stepsPerCycle) {
+		super(id,location,stepsPerCycle);
+	}
+}
---
+++ Game/src/model/units/MedicalUnit.java
@@ -0,0 +1,12 @@
+package model.units;
+import simulation.Address;
+
+public abstract class MedicalUnit extends Unit {
+	int healingAmount;
+	int treatmentAmount;
+	public MedicalUnit(String id, Address location, int stepsPerCycle){
+		super(id,location,stepsPerCycle);
+		healingAmount=10;
+		treatmentAmount=10;
+	}
+}
